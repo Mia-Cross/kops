cf508cd261be768eaf4d0ac6bd1a48b53d4594b1
Leïla MARABESE <lmarabese@scaleway.com>
2022-07-27 18:07:27 +0200

    [WIP] writing model/tasks for instance and volume

--- pkg/model/scalewaymodel/instances.go
+++ pkg/model/scalewaymodel/instances.go
@@ -3,6 +3,7 @@ package scalewaymodel
 import (
 	"k8s.io/kops/pkg/model"
 	"k8s.io/kops/upup/pkg/fi"
+	"k8s.io/kops/upup/pkg/fi/cloudup/scalewaytasks"
 )
 
 // InstanceModelBuilder configures instances for the cluster
@@ -16,28 +17,30 @@ type InstanceModelBuilder struct {
 var _ fi.ModelBuilder = &InstanceModelBuilder{}
 
 func (d *InstanceModelBuilder) Build(c *fi.ModelBuilderContext) error {
-	//for _, ig := range d.InstanceGroups {
-	//	name := d.AutoscalingGroupName(ig)
-	//
-	//	var instance scalewaytasks.Instance
-	//	instance.Count = int(fi.Int32Value(ig.Spec.MinSize))
-	//	instance.Name = fi.String(name)
-	//
-	//	// during alpha support we only allow 1 region
-	//	// validation for only 1 region is done at this point
-	//	instance.Zone = fi.String(d.Cluster.Spec.Subnets[0].Zone)
-	//	instance.CommercialType = fi.String(ig.Spec.MachineType)
-	//	instance.Image = fi.String(ig.Spec.Image)
-	//	instance.Tags = []string{"instance-group="+ig.Name} //TODO(jtherin): find a better way
-	//
-	//	userData, err := d.BootstrapScriptBuilder.ResourceNodeUp(c, ig)
-	//	if err != nil {
-	//
-	//		return err
-	//	}
-	//	instance.UserData = userData
-	//
-	//	c.AddTask(&instance)
-	//}
+	for _, ig := range d.InstanceGroups {
+		name := d.AutoscalingGroupName(ig)
+
+		instance := scalewaytasks.Instance{
+			Count:     int(fi.Int32Value(ig.Spec.MinSize)),
+			Name:      fi.String(name),
+			Lifecycle: d.Lifecycle,
+
+			// during alpha support we only allow 1 region
+			// validation for only 1 region is done at this point
+			Zone:           fi.String(d.Cluster.Spec.Subnets[0].Zone),
+			CommercialType: fi.String(ig.Spec.MachineType),
+			Image:          fi.String(ig.Spec.Image),
+			Tags:           []string{"instance-group=" + ig.Name}, //TODO(jtherin): find a better way
+		}
+
+		userData, err := d.BootstrapScriptBuilder.ResourceNodeUp(c, ig)
+		if err != nil {
+
+			return err
+		}
+		instance.UserData = &userData
+
+		c.AddTask(&instance)
+	}
 	return nil
 }
